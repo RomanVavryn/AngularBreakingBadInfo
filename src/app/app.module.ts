7f599c919fc39a0080f6513e04eca499d0c1d37b
Roman Vavrynkevych <roman.vavrun@gmail.com>
2021-07-23 10:35:52 +0300

    created basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { CharactersPageComponent } from './characters-page/characters-page.component';
+import { EpisodesPageComponent } from './episodes-page/episodes-page.component';
+import { QuotePageComponent } from './quote-page/quote-page.component';
+import { DeathsPageComponent } from './deaths-page/deaths-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    CharactersPageComponent,
+    EpisodesPageComponent,
+    QuotePageComponent,
+    DeathsPageComponent,
+    NotFoundPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/deaths-page/deaths-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deaths-page',
+  templateUrl: './deaths-page.component.html',
+  styleUrls: ['./deaths-page.component.scss']
+})
+export class DeathsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
