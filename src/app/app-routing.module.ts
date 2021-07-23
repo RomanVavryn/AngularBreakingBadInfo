4475ba8f91c494d5f28edb959d001ecc58b2b547
Roman Vavrynkevych <roman.vavrun@gmail.com>
2021-07-23 11:28:19 +0300

    implemented navigation & basic app, home-page component structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from "./home-page/home-page.component";
+import {CharactersPageComponent} from "./characters-page/characters-page.component";
+import {EpisodesPageComponent} from "./episodes-page/episodes-page.component";
+import {QuotePageComponent} from "./quote-page/quote-page.component";
+import {DeathsPageComponent} from "./deaths-page/deaths-page.component";
+import {NotFoundPageComponent} from "./not-found-page/not-found-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'characters', component: CharactersPageComponent},
+  {path: 'episodes', component: EpisodesPageComponent},
+  {path: 'quote', component: QuotePageComponent},
+  {path: 'deaths', component: DeathsPageComponent},
+  {path: '404', component: NotFoundPageComponent},
+  {path: '**', redirectTo: '/404'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
