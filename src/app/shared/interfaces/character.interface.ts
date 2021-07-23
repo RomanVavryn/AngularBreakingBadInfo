d338040a99bdd2734b0615e705af5cfbf95002fe
Roman Vavrynkevych <roman.vavrun@gmail.com>
2021-07-24 00:19:45 +0300

    implemented quotes, characters interfaces

---
+++ src/app/shared/interfaces/character.interface.ts
@@ -0,0 +1,13 @@
+export interface CharacterInterface {
+  appearance: number[];
+  better_call_saul_appearance: [];
+  birthday: string;
+  category: string[] | string;
+  char_id: number;
+  img: string;
+  name: string;
+  nickname: string;
+  occupation: string[];
+  portrayed: string;
+  status: string | boolean;
+}
---
+++ src/app/shared/interfaces/quote.interface.ts
@@ -0,0 +1,6 @@
+export interface QuoteInterface {
+  author: string;
+  quote: string;
+  quote_id: number;
+  series: string;
+}
