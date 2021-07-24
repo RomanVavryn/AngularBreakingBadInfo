c21f3f98ed6586d3096c709138ce26809549d624
Roman Vavrynkevych <roman.vavrun@gmail.com>
2021-07-24 19:15:35 +0300

    death.component(page) & interface, made basic version

--- src/app/deaths-page/deaths-page.component.ts
+++ src/app/deaths-page/deaths-page.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from "rxjs";
+import {HttpService} from "../shared/http.service";
+import {DeathInterface} from "../shared/interfaces/death.interface";
 
 @Component({
   selector: 'app-deaths-page',
   templateUrl: './deaths-page.component.html',
   styleUrls: ['./deaths-page.component.scss']
 })
-export class DeathsPageComponent implements OnInit {
+export class DeathsPageComponent implements OnInit, OnDestroy {
+  deathsLoaded: boolean = false;
+  deathsSub: Subscription | undefined;
+  deathsArr: DeathInterface[] | undefined;
 
-  constructor() { }
+  constructor(private http: HttpService) { }
 
   ngOnInit(): void {
+    this.deathsSub = this.http.getAllDeaths()
+      .subscribe((deaths: DeathInterface[]) => {
+          this.deathsArr = deaths;
+          this.deathsLoaded = true;
+        },
+        error => {
+          console.log('something went wrong!');
+          console.error(error);
+        }
+      )
+  }
+
+  ngOnDestroy(): void {
+    this.deathsSub?.unsubscribe();
   }
 
 }
---
+++ src/app/shared/interfaces/death.interface.ts
@@ -0,0 +1,10 @@
+export interface DeathInterface {
+  cause: string; // причина смерті
+  death: string; // кого вбили
+  death_id: number;
+  episode: number;
+  last_words: string;
+  number_of_deaths: number; // кількість попередніх вбивст
+  responsible: string; // вбивця
+  season: number;
+}
