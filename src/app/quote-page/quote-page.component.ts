9a4364bda79989caf307633015ea30595503e6c3
Roman Vavrynkevych <roman.vavrun@gmail.com>
2021-07-24 19:14:51 +0300

    quote.component made basic version

--- src/app/quote-page/quote-page.component.ts
+++ src/app/quote-page/quote-page.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from "rxjs";
+import {QuoteInterface} from "../shared/interfaces/quote.interface";
+import {HttpService} from "../shared/http.service";
 
 @Component({
   selector: 'app-quote-page',
   templateUrl: './quote-page.component.html',
   styleUrls: ['./quote-page.component.scss']
 })
-export class QuotePageComponent implements OnInit {
+export class QuotePageComponent implements OnInit, OnDestroy {
+  quotesLoaded: boolean = false;
+  quotesSub: Subscription | undefined;
+  quoteArr: QuoteInterface[] | undefined;
 
-  constructor() { }
+  constructor(private http: HttpService) { }
 
   ngOnInit(): void {
+    this.quotesSub = this.http.getAllQuotes()
+      .subscribe((quotes: QuoteInterface[]) => {
+          this.quoteArr = quotes;
+          this.quotesLoaded = true;
+        },
+        error => {
+          console.log('something went wrong!');
+          console.error(error);
+        }
+      )
   }
 
+  ngOnDestroy(): void {
+    this.quotesSub?.unsubscribe();
+  }
 }
